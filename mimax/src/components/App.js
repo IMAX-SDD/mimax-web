62a6737c4c403b973afeb45c293e439fc03bd3b1
aaselvam <selvaa@rpi.edu>
2022-09-27 18:56:31 -0400

    Feature: Add button that calls TMDb API

--- mimax/src/components/App.js
+++ mimax/src/components/App.js
@@ -12,12 +12,20 @@ function App() {
     setCount(0);
   }
 
+  const callAPI = () => {
+    fetch('https://api.themoviedb.org/3/movie/550?api_key=9e6293836bcabd02d80d27ccca8eb072', { method: 'GET' })
+    .then(data => data.json()) // Parsing the data into a JavaScript object
+    .then(json => alert(JSON.stringify(json))) // Displaying the stringified data in an alert popup
+  }
+
   return (
     <div class="App">
       <h1>AWESOME BUTTON CLICKER</h1>
       <h2>{count}</h2>
       <button class="btn" onClick={increment}>CLICK ME</button> <br/>
-      <button class="btn" style={{marginTop: '20px'}} onClick={reset}>RESET</button>
+      <button class="btn" style={{marginTop: '20px'}} onClick={reset}>RESET</button> <br/> <br/>
+      <button class="btn" onClick={callAPI}>API CALL</button>
+      <p ></p>
     </div>
   );
 }
