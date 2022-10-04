77bbae329d4b644a1a10e009a7b55d74e70d9193
aaselvam <selvaa@rpi.edu>
2022-10-04 14:04:00 -0400

    Functional Search Bar

--- mimax/src/components/Homepage/Main.js
+++ mimax/src/components/Homepage/Main.js
@@ -4,6 +4,10 @@ import { useState } from "react";
 const Main = () => {
 
     const [count, setCount] = useState(0);
+    const [term, setTerm] = useState("");
+    const [movieTitle, setMovieTitle] = useState("");
+    const [movieSynopsis, setMovieSynopsis] = useState("");
+    const [movieScore, setMovieScore] = useState("");
 
     const increment = () => {
         setCount(count + 1);
@@ -13,10 +17,22 @@ const Main = () => {
         setCount(0);
     }
 
+    function setSearch(e) {
+        setTerm(e.target.value);
+    }
+
+    function setMovieDeets(json) {
+        setMovieTitle(json["original_title"]);
+        setMovieSynopsis(json["overview"]);
+        setMovieScore("Score " + json["vote_average"]);
+    }
+
     const callAPI = () => {
-        fetch('https://api.themoviedb.org/3/movie/550?api_key=9e6293836bcabd02d80d27ccca8eb072', { method: 'GET' })
+        console.log(term);
+        var link = "https://api.themoviedb.org/3/search/movie?api_key=9e6293836bcabd02d80d27ccca8eb072&query='" + term + "'"
+        fetch(link, { method: 'GET' })
         .then(data => data.json()) // Parsing the data into a JavaScript object
-        .then(json => alert(JSON.stringify(json))) // Displaying the stringified data in an alert popup
+        .then(json => setMovieDeets(json["results"][0])) // Displaying the stringified data in an alert popup
     }
 
     return (
@@ -26,11 +42,13 @@ const Main = () => {
                 <h2>{count}</h2>
                 <button class="btn" onClick={increment}>CLICK ME</button> <br/>
                 <button class="btn" style={{marginTop: '20px'}} onClick={reset}>RESET</button> <br/> <br/>
-                <button class="btn" onClick={callAPI}>API CALL</button> <br/>
-                <input type="search" placeholder="Search" class="search-field"/>
-                <button type="submit">
+                <input type="search" placeholder="Search Movies" onChange={setSearch} class="search-field"/>
+                <button type="submit" onClick={callAPI}>
                     <i class="fa fa-search fa-lg"/>
                 </button>
+                <h2>{movieTitle}</h2>
+                <p>{movieSynopsis}</p>
+                <h3>{movieScore}</h3>
             </div>
 
             <div className="director-actor-bio-section">
