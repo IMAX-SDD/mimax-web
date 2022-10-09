f847aa697a2fb73e2519fbd7768082bd087174f1
rren8957 <ruonren@gmail.com>
2022-10-09 04:04:32 -0400

    Implemented sidebar menu
    When you click on the hamburger icon, the sidebar menu is now flushed to the left side of the page instead of below the header.

--- mimax/src/components/Homepage/Navbar.js
+++ mimax/src/components/Homepage/Navbar.js
@@ -1,25 +1,35 @@
 import React from "react";
 import logo from "../img/logo.jpg"
 import { FaBars } from 'react-icons/fa';
+import { FaTimes } from 'react-icons/fa';
 
 const Navbar = () => {
     const [toggleBar, setToggleBar] = React.useState(false);
 
-    const barLinks = (
-        <div className="links">
+    const sidebar = (
+        <div className="sidebar">
+            <button className='close-sidebar-btn' onClick={() => setToggleBar(!toggleBar)}>
+                <FaTimes />
+            </button>
             <ul className="bar-links">
                 <li>
-                    <h2>...</h2>
+                    <a href=''>Movies</a>
                 </li>
                 <li>
-                    <h2>...</h2>
+                    <a href=''>TV Shows</a>
+                </li>
+                <li>
+                    <a href=''>Actors</a>
+                </li>
+                <li>
+                    <a href=''>Directors</a>
                 </li>
             </ul>
         </div>
     ) 
     return ( 
         <nav>
-            <div className="nav-center">
+            <div className={toggleBar ? "background-dark" : "background" }>
                 <div className="nav-header">
                     <button className="nav-bar"
                             onClick={() => setToggleBar(!toggleBar)}>
@@ -27,7 +37,7 @@ const Navbar = () => {
                     </button>
                     <img className="logo" src={logo} alt="logo"/>                    
                 </div>
-                {toggleBar && barLinks}
+                {toggleBar && sidebar}
             </div>
         </nav>
     )
