9e9a6b93519741e966bad812f831efb4748240f5
aaselvam <selvaa@rpi.edu>
2022-11-29 17:22:44 -0500

    actor page fix

--- mimax/src/components/HomePage/HomePageRouter.js
+++ mimax/src/components/HomePage/HomePageRouter.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route }
   from 'react-router-dom';
-import ActorsPageMain from '../ActorsPage/ActorsPageMain';
 import DirectorsPageMain from '../DirectorsPage/DirectorsPageMain';
 import HomePageMain from './HomePageMain';
 import MoviesPageMain from '../MoviesPage/MoviesPageMain';
@@ -10,6 +9,7 @@ import MoviesListMain from '../MoviesList/MoviesListMain';
 import MoviesListReverse from '../MoviesListReverse/MoviesListReverseMain';
 import TVListMain from '../TVList/TVListMain';
 import TVShowsPageMain from '../TVShowsPage/TVShowsPageMain';
+import ActorsListMain from '../ActorsPage/ActorsListMain';
 
 // routing for home page
 // do we need className="Homepage", looks like we don't?
@@ -20,7 +20,7 @@ function HomePageRouter() {
         <Navbar />
         <Routes>
           <Route path="/" element={<HomePageMain />} />
-          <Route path="/actors" element={<ActorsPageMain />} />
+          <Route path="/actors" element={<ActorsListMain />} />
           <Route path="/directors" element={<DirectorsPageMain />} />
           <Route path="/movieslist" element={<MoviesListMain />} />
           <Route path="/movies" element={<MoviesPageMain />} />
